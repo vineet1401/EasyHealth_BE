7511c5a08d5b251816ea0abd2a64e76d2c9825fa
vineet1401 <sakpalvineet1401@gmail.com>
2024-08-17 10:53:57 +0530

    Defined Cors

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ const notificationRouter = require("./routes/notificationRouter");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin:"https://easy-health-sigma.vercel.app/",
+  methods:["GET", "POST", "PUT", "DELETE", "PATCH"]
+}));
 app.use(express.json());
 app.use("/api/user", userRouter);
 app.use("/api/doctor", doctorRouter);
